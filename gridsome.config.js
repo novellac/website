93d83fe92f8552a7859946304551b5d30ceccf52
Novella C <novella@altodev.net>
2020-08-08 16:54:07 -0400

    Add packages

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,6 +5,16 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Gridsome',
-  plugins: []
-}
+  siteName: "N Den",
+  siteDescription: "N Portfolio and Ideas Site",
+  plugins: [
+    {
+      use: "@gridsome/source-filesystem",
+      options: {
+        path: "content/ideas/**/*.md",
+        typeName: "IdeaPost",
+        route: "/ideas/:title",
+      },
+    },
+  ],
+};
