ae933f046d058681cd76b0a19f730324a9220a80
Novella C <novella@altodev.net>
2020-08-10 19:04:09 -0400

    Replace old addCollection with new actions format

--- gridsome.server.js
+++ gridsome.server.js
@@ -5,12 +5,12 @@
 // Changes here require a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
-module.exports = function (api) {
-  api.loadSource(({ addCollection }) => {
-    // Use the Data Store API here: https://gridsome.org/docs/data-store-api/
-  })
+module.exports = function(api) {
+  api.loadSource((actions) => {
+    // Use Data Store API here
+  });
 
   api.createPages(({ createPage }) => {
     // Use the Pages API here: https://gridsome.org/docs/pages-api/
-  })
-}
+  });
+};
