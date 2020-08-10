c5451b0f69eb39e4275812437be041cee1cea74a
Novella C <novella@altodev.net>
2020-08-10 19:05:51 -0400

    Add landing page for Ideas

---
+++ src/pages/Ideas.vue
@@ -0,0 +1,63 @@
+<template>
+  <Layout>
+    <!-- Learn how to use images here: https://gridsome.org/docs/images -->
+    <g-image alt="Example image" src="~/favicon.png" width="135" />
+
+    <h1>Hello, world!</h1>
+
+    <p>
+      Other text soon to come
+    </p>
+    <section class="posts">
+      <IdeaList
+        v-for="edge in $page.allIdeaPost.edges"
+        :key="edge.node.id"
+        :ideaPost="edge.node"
+      />
+    </section>
+    <p class="home-links">
+      <a href="https://gridsome.org/docs/" target="_blank" rel="noopener"
+        >Gridsome Docs</a
+      >
+      <a
+        href="https://github.com/gridsome/gridsome"
+        target="_blank"
+        rel="noopener"
+        >GitHub</a
+      >
+    </p>
+  </Layout>
+</template>
+<page-query>
+query {
+  allIdeaPost {
+    totalCount
+    edges {
+      node {
+        id
+        title
+        path
+        experiencelevels
+        techstack
+      }
+    }
+  }
+}
+</page-query>
+<script>
+import IdeaList from "@/components/IdeaList.vue";
+export default {
+  components: {
+    IdeaList,
+  },
+  metaInfo: {
+    title: "Hello, world!",
+  },
+};
+</script>
+
+<style>
+.home-links a {
+  margin-right: 1rem;
+}
+</style>
